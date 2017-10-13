9819f5ecf0118fdebdc9da4ba47657f17b884155
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-10-13 21:02:38 +0200

    Add graph section, Remove slider, Add font

--- js/main.js
+++ js/main.js
@@ -1,51 +1,24 @@
 (function() {
-  var installButton = document.getElementById('js-install-button')
+  var installButtons = document.getElementsByClassName('js-install-button')
 
-  if (chrome.app.isInstalled) {
-    installButton.innerHTML = 'INSTALLED, THANKS!'
-    installButton.className += ' installed-button'
+  for (var i = 0; i < installButtons.length; i++) {
+    var installButton = installButtons[i]
 
-  } else {
-    installButton.addEventListener('click', function(event) {
-      var href = this.href
+    if (chrome.app.isInstalled) {
+      installButton.innerHTML = 'INSTALLED, THANKS!'
+      installButton.className += ' installed-button'
 
-      event.preventDefault()
+    } else {
+      installButton.addEventListener('click', function(event) {
+        var href = this.href
 
-      chrome.webstore.install(href, function() {}, function() {
-        console.log('Whops, inline install failed, continue with default link')
-        window.location = href
-      })
-    }, false)
-  }
+        event.preventDefault()
 
-  var timeoutId = null
-  var DURATION = 550
-
-  var siema = new Siema({
-    selector: '.siema',
-    duration: DURATION,
-    easing: 'ease-out',
-    perPage: 1,
-    startIndex: 0,
-    draggable: true,
-    multipleDrag: true,
-    threshold: 20,
-    loop: true,
-    onInit: function() {},
-    onChange: function() {
-      clearTimeout(timeoutId)
+        chrome.webstore.install(href, function() {}, function() {
+          console.log('Whops, inline install failed, continue with default link')
+          window.location = href
+        })
+      }, false)
     }
-  });
-
-  function autoloop() {
-    timeoutId = setTimeout(function() {
-      siema.next()
-      setTimeout(autoloop, DURATION)
-    }, 3000)
   }
-
-  autoloop()
-
-  document.getElementById('js-prev').addEventListener('click', function() { siema.prev() })
-  document.getElementById('js-next').addEventListener('click', function() { siema.next() })
 })()
